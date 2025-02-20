50ae7ebe0f996a18161afd182d8fd0c39af9ceff
Bureau of Emergency Medical and Trauma Services <nicolas.foss@hhs.iowa.gov>
2025-02-20 12:17:17 -0600

    update ttr-01 .Rd files

--- R/ttr_01_population.R
+++ R/ttr_01_population.R
@@ -59,6 +59,79 @@
 #' * a tibble for the initial population
 #' * a tibble for the total dataset with computations
 #'
+#' @examples
+#' # create tables to test correct functioning
+#'
+#'   # patient table
+#'   patient_table <- tibble::tibble(
+#'
+#'     erecord_01 = c("R1", "R2", "R3", "R4", "R5"),
+#'     incident_date = as.Date(c("2025-01-01", "2025-01-05", "2025-02-01", "2025-01-01", "2025-06-01")),
+#'     patient_dob = as.Date(c("2000-01-01", "2020-01-01", "2023-02-01", "2023-01-01", "1970-06-01")),
+#'     epatient_15 = c(25, 5, 2, 2, 55),  # Ages
+#'     epatient_16 = c("Years", "Years", "Years", "Years", "Years")
+#'
+#'   )
+#'
+#'   # response table
+#'   response_table <- tibble::tibble(
+#'
+#'     erecord_01 = c("R1", "R2", "R3", "R4", "R5"),
+#'     eresponse_05 = rep(2205001, 5),
+#'   )
+#'
+#'   # arrest table
+#'   arrest_table <- tibble::tibble(
+#'
+#'     erecord_01 = c("R1", "R2", "R3", "R4", "R5"),
+#'     earrest_01 = rep("No", 5)
+#'   )
+#'
+#'   # vitals table
+#'   vitals_table <- tibble::tibble(
+#'
+#'     erecord_01 = c("R1", "R2", "R3", "R4", "R5"),
+#'     evitals_06 = c(100, 90, 80, 70, 85),
+#'     evitals_07 = c(80, 90, 50, 60, 87),
+#'     evitals_10 = c(110, 89, 88, 71, 85),
+#'     evitals_12 = c(50, 60, 70, 80, 75),
+#'     evitals_14 = c(30, 9, 8, 7, 31),
+#'     evitals_23 = c(6, 7, 8, 9, 10),
+#'     evitals_26 = c(3326007, 3326005, 3326003, 3326001, 3326007),
+#'   )
+#'
+#'   # disposition table
+#'   disposition_table <- tibble::tibble(
+#'     erecord_01 = c("R1", "R2", "R3", "R4", "R5"),
+#'     edisposition_30 = c(4230013, 4230009, 4230013, 4230009, 4230013)
+#'   )
+#'
+#'   # test the success of the function
+#'   result <- ttr_01_population(patient_scene_table = patient_table,
+#'                         response_table = response_table,
+#'                         arrest_table = arrest_table,
+#'                         vitals_table = vitals_table,
+#'                         disposition_table = disposition_table,
+#'                         erecord_01_col = erecord_01,
+#'                         incident_date_col = incident_date,
+#'                         patient_DOB_col = patient_dob,
+#'                         epatient_15_col = epatient_15,
+#'                         epatient_16_col = epatient_16,
+#'                         eresponse_05_col = eresponse_05,
+#'                         earrest_01_col = earrest_01,
+#'                         evitals_06_col = evitals_06,
+#'                         evitals_07_col = evitals_07,
+#'                         evitals_10_col = evitals_10,
+#'                         evitals_12_col = evitals_12,
+#'                         evitals_14_col = evitals_14,
+#'                         evitals_23_col = evitals_23,
+#'                         evitals_26_col = evitals_26,
+#'                         transport_disposition_col = edisposition_30
+#'                    )
+#'
+#' # show the results of filtering at each step
+#' result$filter_process
+#'
 #' @author Nicolas Foss, Ed.D., MS
 #'
 #' @export
