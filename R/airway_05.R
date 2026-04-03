10f1a05235ae6ccd6fdc6ce78d149c9d643bb40a
bemts-hhs <nicolas.foss@hhs.iowa.gov>
2026-04-03 15:08:15 -0500

    add IDE navigation for Positron and RStudio to airway_05 functions

--- R/airway_05.R
+++ R/airway_05.R
@@ -156,84 +156,86 @@
 #'
 #' @export
 #'
-airway_05 <- function(df = NULL,
-                      patient_scene_table = NULL,
-                      response_table = NULL,
-                      arrest_table = NULL,
-                      procedures_table = NULL,
-                      vitals_table = NULL,
-                      erecord_01_col,
-                      incident_date_col = NULL,
-                      patient_DOB_col = NULL,
-                      epatient_15_col,
-                      epatient_16_col,
-                      earrest_01_col,
-                      eresponse_05_col,
-                      evitals_01_col,
-                      evitals_12_col,
-                      eprocedures_01_col,
-                      eprocedures_02_col,
-                      eprocedures_03_col,
-                      confidence_interval = FALSE,
-                      method = c("wilson", "clopper-pearson"),
-                      conf.level = 0.95,
-                      correct = TRUE,
-                      ...) {
-
-  # Set default method and adjustment method
+airway_05 <- function(
+  df = NULL,
+  patient_scene_table = NULL,
+  response_table = NULL,
+  arrest_table = NULL,
+  procedures_table = NULL,
+  vitals_table = NULL,
+  erecord_01_col,
+  incident_date_col = NULL,
+  patient_DOB_col = NULL,
+  epatient_15_col,
+  epatient_16_col,
+  earrest_01_col,
+  eresponse_05_col,
+  evitals_01_col,
+  evitals_12_col,
+  eprocedures_01_col,
+  eprocedures_02_col,
+  eprocedures_03_col,
+  confidence_interval = FALSE,
+  method = c("wilson", "clopper-pearson"),
+  conf.level = 0.95,
+  correct = TRUE,
+  ...
+) {
+  # Set default method and adjustment method ----
   method <- match.arg(method, choices = c("wilson", "clopper-pearson"))
 
-  # utilize applicable tables to analyze the data for the measure
-  if(
-    all(!is.null(patient_scene_table),
-        !is.null(response_table),
-        !is.null(procedures_table),
-        !is.null(vitals_table)
-    ) && is.null(df)
-
+  # utilize applicable tables to analyze the data for the measure ----
+  if (
+    all(
+      !is.null(patient_scene_table),
+      !is.null(response_table),
+      !is.null(procedures_table),
+      !is.null(vitals_table)
+    ) &&
+      is.null(df)
   ) {
-
-    # Start timing the function execution
+    # Start timing the function execution ----
     start_time <- Sys.time()
 
-    # header
+    # header ----
     cli::cli_h1("Airway-05")
 
-    # header
+    # header ----
     cli::cli_h2("Gathering Records for Airway-05")
 
-  #############################################################################
-  #                                                                           #
-  #     Get Population Level Information from tables and columns              #
-  #                                                                           #
-  #############################################################################
+    #############################################################################
+    #                                                                           #
+    #     Get Population Level Information from tables and columns ----         #
+    #                                                                           #
+    #############################################################################
 
-  airway_05_populations <- airway_05_population(patient_scene_table = patient_scene_table,
-                                               response_table = response_table,
-                                               arrest_table = arrest_table,
-                                               procedures_table = procedures_table,
-                                               vitals_table = vitals_table,
-                                               erecord_01_col = {{ erecord_01_col }},
-                                               incident_date_col = {{ incident_date_col }},
-                                               patient_DOB_col = {{ patient_DOB_col }},
-                                               epatient_15_col = {{ epatient_15_col }},
-                                               epatient_16_col = {{ epatient_16_col }},
-                                               earrest_01_col = {{ earrest_01_col }},
-                                               eresponse_05_col = {{ eresponse_05_col }},
-                                               evitals_01_col = {{ evitals_01_col }},
-                                               evitals_12_col = {{ evitals_12_col }},
-                                               eprocedures_01_col = {{ eprocedures_01_col }},
-                                               eprocedures_02_col = {{ eprocedures_02_col }},
-                                               eprocedures_03_col = {{ eprocedures_03_col }}
-                                               )
+    airway_05_populations <- airway_05_population(
+      patient_scene_table = patient_scene_table,
+      response_table = response_table,
+      arrest_table = arrest_table,
+      procedures_table = procedures_table,
+      vitals_table = vitals_table,
+      erecord_01_col = {{ erecord_01_col }},
+      incident_date_col = {{ incident_date_col }},
+      patient_DOB_col = {{ patient_DOB_col }},
+      epatient_15_col = {{ epatient_15_col }},
+      epatient_16_col = {{ epatient_16_col }},
+      earrest_01_col = {{ earrest_01_col }},
+      eresponse_05_col = {{ eresponse_05_col }},
+      evitals_01_col = {{ evitals_01_col }},
+      evitals_12_col = {{ evitals_12_col }},
+      eprocedures_01_col = {{ eprocedures_01_col }},
+      eprocedures_02_col = {{ eprocedures_02_col }},
+      eprocedures_03_col = {{ eprocedures_03_col }}
+    )
 
-    # create a separator
+    # create a separator ----
     cli::cli_text("\n")
 
-    # header for calculations
+    # header for calculations ----
     cli::cli_h2("Calculating Airway-05")
 
-    # summary
+    # summary ----
     airway.05 <- results_summarize(
       total_population = NULL,
       adult_population = airway_05_populations$adults,
@@ -248,87 +250,90 @@ airway_05 <- function(df = NULL,
       ...
     )
 
-    # create a separator
+    # create a separator ----
     cli::cli_text("\n")
 
-    # Calculate and display the runtime
+    # Calculate and display the runtime ----
     end_time <- Sys.time()
     run_time_secs <- difftime(end_time, start_time, units = "secs")
     run_time_secs <- as.numeric(run_time_secs)
 
     if (run_time_secs >= 60) {
-
-      run_time <- round(run_time_secs / 60, 2)  # Convert to minutes and round
-      cli::cli_alert_success("Function completed in {cli::col_green(paste0(run_time, 'm'))}.")
-
+      run_time <- round(run_time_secs / 60, 2) # Convert to minutes and round
+      cli::cli_alert_success(
+        "Function completed in {cli::col_green(paste0(run_time, 'm'))}."
+      )
     } else {
-
-      run_time <- round(run_time_secs, 2)  # Keep in seconds and round
-      cli::cli_alert_success("Function completed in {cli::col_green(paste0(run_time, 's'))}.")
-
+      run_time <- round(run_time_secs, 2) # Keep in seconds and round
+      cli::cli_alert_success(
+        "Function completed in {cli::col_green(paste0(run_time, 's'))}."
+      )
     }
 
-    # create a separator
+    # create a separator ----
     cli::cli_text("\n")
 
-    # when confidence interval is "wilson", check for n < 10
+    # when confidence interval is "wilson", check for n < 10 ----
     # to warn about incorrect Chi-squared approximation
-    if (any(airway.05$denominator < 10) && method == "wilson" && confidence_interval) {
-
-      cli::cli_warn("In {.fn prop.test}: Chi-squared approximation may be incorrect for any n < 10.")
-
+    if (
+      any(airway.05$denominator < 10) &&
+        method == "wilson" &&
+        confidence_interval
+    ) {
+      cli::cli_warn(
+        "In {.fn prop.test}: Chi-squared approximation may be incorrect for any n < 10."
+      )
     }
 
     return(airway.05)
-
-    } else if(
-    all(is.null(patient_scene_table),
-        is.null(response_table),
-        is.null(procedures_table),
-        is.null(vitals_table),
-        is.null(arrest_table)
-
-    ) && !is.null(df)
-
+  } else if (
+    all(
+      is.null(patient_scene_table),
+      is.null(response_table),
+      is.null(procedures_table),
+      is.null(vitals_table),
+      is.null(arrest_table)
+    ) &&
+      !is.null(df)
   ) {
-
-    # Start timing the function execution
+    # Start timing the function execution ----
     start_time <- Sys.time()
 
-    # header
+    # header ----
     cli::cli_h1("Airway-05")
 
-    # header
+    # header ----
     cli::cli_h2("Gathering Records for Airway-05")
 
-  #############################################################################
-  #                                                                           #
-  #     Get Population Level Information from tables and columns              #
-  #                                                                           #
-  #############################################################################
+    #############################################################################
+    #                                                                           #
+    #     Get Population Level Information from tables and columns ----         #
+    #                                                                           #
+    #############################################################################
 
-    airway_05_populations <- airway_05_population(df = df,
-                                               erecord_01_col = {{ erecord_01_col }},
-                                               incident_date_col = {{ incident_date_col }},
-                                               patient_DOB_col = {{ patient_DOB_col }},
-                                               epatient_15_col = {{ epatient_15_col }},
-                                               epatient_16_col = {{ epatient_16_col }},
-                                               earrest_01_col = {{ earrest_01_col }},
-                                               eresponse_05_col = {{ eresponse_05_col }},
-                                               evitals_01_col = {{ evitals_01_col }},
-                                               evitals_12_col = {{ evitals_12_col }},
-                                               eprocedures_01_col = {{ eprocedures_01_col }},
-                                               eprocedures_02_col = {{ eprocedures_02_col }},
-                                               eprocedures_03_col = {{ eprocedures_03_col }}
-                                               )
+    airway_05_populations <- airway_05_population(
+      df = df,
+      erecord_01_col = {{ erecord_01_col }},
+      incident_date_col = {{ incident_date_col }},
+      patient_DOB_col = {{ patient_DOB_col }},
+      epatient_15_col = {{ epatient_15_col }},
+      epatient_16_col = {{ epatient_16_col }},
+      earrest_01_col = {{ earrest_01_col }},
+      eresponse_05_col = {{ eresponse_05_col }},
+      evitals_01_col = {{ evitals_01_col }},
+      evitals_12_col = {{ evitals_12_col }},
+      eprocedures_01_col = {{ eprocedures_01_col }},
+      eprocedures_02_col = {{ eprocedures_02_col }},
+      eprocedures_03_col = {{ eprocedures_03_col }}
+    )
 
-    # create a separator
+    # create a separator <-
     cli::cli_text("\n")
 
-    # header for calculations
+    # header for calculations ----
     cli::cli_h2("Calculating Airway-05")
 
-    # summary
+    # summary ----
     airway.05 <- results_summarize(
       adult_population = airway_05_populations$adults,
       peds_population = airway_05_populations$peds,
@@ -342,38 +347,41 @@ airway_05 <- function(df = NULL,
       ...
     )
 
-    # create a separator
+    # create a separator ----
     cli::cli_text("\n")
 
-    # Calculate and display the runtime
+    # Calculate and display the runtime ----
     end_time <- Sys.time()
     run_time_secs <- difftime(end_time, start_time, units = "secs")
     run_time_secs <- as.numeric(run_time_secs)
 
     if (run_time_secs >= 60) {
-
-      run_time <- round(run_time_secs / 60, 2)  # Convert to minutes and round
-      cli::cli_alert_success("Function completed in {cli::col_green(paste0(run_time, 'm'))}.")
-
+      run_time <- round(run_time_secs / 60, 2) # Convert to minutes and round
+      cli::cli_alert_success(
+        "Function completed in {cli::col_green(paste0(run_time, 'm'))}."
+      )
     } else {
-
-      run_time <- round(run_time_secs, 2)  # Keep in seconds and round
-      cli::cli_alert_success("Function completed in {cli::col_green(paste0(run_time, 's'))}.")
-
+      run_time <- round(run_time_secs, 2) # Keep in seconds and round
+      cli::cli_alert_success(
+        "Function completed in {cli::col_green(paste0(run_time, 's'))}."
+      )
     }
 
-    # create a separator
+    # create a separator ----
     cli::cli_text("\n")
 
-    # when confidence interval is "wilson", check for n < 10
+    # when confidence interval is "wilson", check for n < 10 ----
     # to warn about incorrect Chi-squared approximation
-    if (any(airway.05$denominator < 10) && method == "wilson" && confidence_interval) {
-
-      cli::cli_warn("In {.fn prop.test}: Chi-squared approximation may be incorrect for any n < 10.")
-
+    if (
+      any(airway.05$denominator < 10) &&
+        method == "wilson" &&
+        confidence_interval
+    ) {
+      cli::cli_warn(
+        "In {.fn prop.test}: Chi-squared approximation may be incorrect for any n < 10."
+      )
     }
 
     return(airway.05)
-
-    }
+  }
 }
