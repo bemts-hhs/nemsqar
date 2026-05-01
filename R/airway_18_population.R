f8d070349f5dc25b3fa29559a1442f6502d849a1
bemts-hhs <nicolas.foss@hhs.iowa.gov>
2026-05-01 14:46:05 -0500

    add the missingness summary to airway_18_population

--- R/airway_18_population.R
+++ R/airway_18_population.R
@@ -22,6 +22,7 @@
 #' * a tibble for each population of interest
 #' * a tibble for the initial population
 #' * a tibble for the total dataset with computations
+#' * a tibble with a summary of missingness for each column in each table
 #'
 #' @examples
 #'
@@ -219,6 +220,19 @@ airway_18_population <- function(
     type = "tasks",
     clear = FALSE,
     format = "{cli::pb_name} [Working on {cli::pb_current} of {cli::pb_total} tasks] {cli::pb_bar} | {cli::col_blue('Progress')}: {cli::pb_percent} | {cli::col_blue('Runtime')}: [{cli::pb_elapsed}]"
+  )
+
+  ###___________________________________________________________________________
+  # Estimate missingness in each table for included columns in the measure
+  ###___________________________________________________________________________
+
+  # utilize the internal `nemsqa_missing_summary` to estimate missingness
+  missings <- nemsqa_missing_summary(
+    patient_scene_table,
+    response_table,
+    airway_table,
+    procedures_table,
+    vitals_table
   )
 
   # progress update, these will be repeated throughout the script ----
@@ -1114,7 +1128,8 @@ airway_18_population <- function(
     adults = adult_pop,
     peds = peds_pop,
     initial_population = initial_population,
-    computing_population = computing_population
+    computing_population = computing_population,
+    missingness = missings
   )
 
   cli::cli_progress_done(id = progress_bar_population)
